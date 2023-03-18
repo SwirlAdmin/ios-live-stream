0fd8bca0880f749353fac49b8d3d76e5d3cb3806
Pinkesh Gajjar <pinkeshmgajjar@gmail.com>
2023-03-19 00:12:21 +0530

    add lib

---
+++ MaccoValidator/MaccoValidator/Validator.swift
@@ -0,0 +1,19 @@
+//
+//  Validator.swift
+//  MaccoValidator
+//
+//  Created by Pinkesh Gajjar on 18/03/23.
+//
+
+import Foundation
+
+public struct Validator {
+    
+    public static func validEmail() -> Bool {
+        return true
+    }
+    
+    public static func syaHello() {
+        print("From Hello ...")
+    }
+}

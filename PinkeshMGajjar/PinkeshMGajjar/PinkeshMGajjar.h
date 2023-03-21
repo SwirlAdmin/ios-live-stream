73a3f6e897e781f94be60913768df96afe89e796
Pinkesh Gajjar <pinkeshmgajjar@gmail.com>
2023-03-21 13:46:55 +0530

    Add another library

---
+++ PinkeshMGajjar/PinkeshMGajjar/PinkeshMGajjar.h
@@ -0,0 +1,18 @@
+//
+//  PinkeshMGajjar.h
+//  PinkeshMGajjar
+//
+//  Created by Pinkesh Gajjar on 21/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinkeshMGajjar.
+FOUNDATION_EXPORT double PinkeshMGajjarVersionNumber;
+
+//! Project version string for PinkeshMGajjar.
+FOUNDATION_EXPORT const unsigned char PinkeshMGajjarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinkeshMGajjar/PublicHeader.h>
+
+

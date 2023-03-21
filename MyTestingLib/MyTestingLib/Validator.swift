83a1a705255d2213a30bdc86481a902664fc59f1
Pinkesh Gajjar <pinkeshmgajjar@gmail.com>
2023-03-21 11:21:35 +0530

    Add Updated Library

---
+++ MyTestingLib/MyTestingLib/Validator.swift
@@ -0,0 +1,16 @@
+//
+//  Validator.swift
+//  MyTestingLib
+//
+//  Created by Pinkesh Gajjar on 21/03/23.
+//
+
+import Foundation
+
+public struct Validator {
+    
+    public static func getString() {
+        
+        print("Hello Validator")
+    }
+}

f88cc079f8154303b84e2dd4f243b04ad6cbbeae
Pinkesh Gajjar <pinkeshmgajjar@gmail.com>
2023-07-05 14:37:34 +0530

    Add Framework

---
+++ WriteMyName/WriteMyName/WriteMyName.h
@@ -0,0 +1,18 @@
+//
+//  WriteMyName.h
+//  WriteMyName
+//
+//  Created by Pinkesh Gajjar on 04/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WriteMyName.
+FOUNDATION_EXPORT double WriteMyNameVersionNumber;
+
+//! Project version string for WriteMyName.
+FOUNDATION_EXPORT const unsigned char WriteMyNameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WriteMyName/PublicHeader.h>
+
+
---
+++ WriteMyName/WriteMyName/WriteName.swift
@@ -0,0 +1,15 @@
+//
+//  WriteName.swift
+//  WriteMyName
+//
+//  Created by Pinkesh Gajjar on 04/07/23.
+//
+
+import Foundation
+
+public struct WriteName {
+    
+    public static func sayHello() {
+        print("From PMGajjar sayHello")
+    }
+}

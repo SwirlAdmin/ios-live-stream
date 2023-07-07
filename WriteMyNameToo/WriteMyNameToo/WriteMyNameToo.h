46205fc47d003f3bf2d5e6d48703c719c47ab01a
Pinkesh Gajjar <pinkeshmgajjar@gmail.com>
2023-07-07 15:24:22 +0530

    Add New Framework

---
+++ WriteMyNameToo/WriteMyNameToo/WriteMyNameToo.h
@@ -0,0 +1,18 @@
+//
+//  WriteMyNameToo.h
+//  WriteMyNameToo
+//
+//  Created by Pinkesh Gajjar on 07/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WriteMyNameToo.
+FOUNDATION_EXPORT double WriteMyNameTooVersionNumber;
+
+//! Project version string for WriteMyNameToo.
+FOUNDATION_EXPORT const unsigned char WriteMyNameTooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WriteMyNameToo/PublicHeader.h>
+
+
---
+++ WriteMyNameToo/WriteMyNameToo/WriteNameToo.swift
@@ -0,0 +1,19 @@
+//
+//  WriteNameToo.swift
+//  WriteMyNameToo
+//
+//  Created by Pinkesh Gajjar on 07/07/23.
+//
+
+import Foundation
+
+public struct WriteNameToo {
+    
+    public static func sayHello() {
+        print("From Pinkesh Gajjar sayHello")
+    }
+    
+    public static func sayHi() {
+        print("From Pinkesh Gajjar sayHi")
+    }
+}
